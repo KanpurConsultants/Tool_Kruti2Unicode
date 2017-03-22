7d7fb5077d4004a358e11f19f66fdcd970f3dd10
meet2arpitg <meet2arpitg@gmail.com>
2017-03-22 15:48:15 +0530

    Init

---
+++ MyTeamApp/FrmKrutiToUnicode.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace MyTeamApp
+{
+    partial class FrmKrutiToUnicode
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.txtFileName = new System.Windows.Forms.TextBox();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLoad.Location = new System.Drawing.Point(239, 144);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(75, 27);
+            this.btnLoad.TabIndex = 5;
+            this.btnLoad.Text = "Load";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // txtFileName
+            // 
+            this.txtFileName.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFileName.Location = new System.Drawing.Point(10, 145);
+            this.txtFileName.Name = "txtFileName";
+            this.txtFileName.Size = new System.Drawing.Size(222, 26);
+            this.txtFileName.TabIndex = 4;
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label13.Location = new System.Drawing.Point(8, 124);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(155, 18);
+            this.label13.TabIndex = 3;
+            this.label13.Text = "Provide excel file : ";
+            this.label13.Click += new System.EventHandler(this.label13_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Brown;
+            this.label1.Location = new System.Drawing.Point(8, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 18);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Note :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Brown;
+            this.label2.Location = new System.Drawing.Point(8, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(354, 18);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Please take backup of excel file before start";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Brown;
+            this.label3.Location = new System.Drawing.Point(8, 91);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(261, 18);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "file may currupt during process.";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Brown;
+            this.label4.Location = new System.Drawing.Point(7, 67);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(369, 18);
+            this.label4.TabIndex = 9;
+            this.label4.Text = " because this tool change the data of that file.";
+            // 
+            // FrmKrutiToUnicode
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(386, 184);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.txtFileName);
+            this.Controls.Add(this.label13);
+            this.Name = "FrmKrutiToUnicode";
+            this.Text = "FrmKrutiToUnicode";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.TextBox txtFileName;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
---
+++ MyTeamApp/FrmKrutiToUnicode.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MyTeamApp
+{
+    public partial class FrmKrutiToUnicode : Form
+    {
+        public FrmKrutiToUnicode()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ExcelDialog = new OpenFileDialog();
+            ExcelDialog.Filter = "Excel Files (*.xlsx) | *.xlsx";
+            ExcelDialog.InitialDirectory = @"C:\";
+            ExcelDialog.Title = "Select your team excel";
+            if (ExcelDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                LibExcel.DB_PATH = ExcelDialog.FileName;
+                txtFileName.Text = ExcelDialog.FileName;
+                txtFileName.ReadOnly = true;
+                txtFileName.Click -= btnLoad_Click;                
+                btnLoad.Enabled = false;
+                LibExcel.InitializeExcel();
+                LibExcel.ConvertKrutiToUnicode();
+                MessageBox.Show("Data converted successfully.");
+            }
+        }
+
+        private void label13_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
